3768e93b351f6ef24bf5f573547397b71a41f06e
Julia Loftin <julia.loftin7@gmail.com>
2024-12-14 20:36:27 -0600

    initial setup

---
+++ src/main/java/com/ReadMoreBetter/ReadMoreBetter/ReadMoreBetterApplication.java
@@ -0,0 +1,13 @@
+package com.ReadMoreBetter.ReadMoreBetter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadMoreBetterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadMoreBetterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ReadMoreBetter/ReadMoreBetter/ReadMoreBetterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ReadMoreBetter.ReadMoreBetter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadMoreBetterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
